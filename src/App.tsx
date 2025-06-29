d8ed4676272549a2c7361604939f5acde35913ee
viriolo <mulavo.bradley@gmail.com>
2025-06-29 20:08:25 +1000

    Fix ServicesPage import error

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { HomePage } from './pages/HomePage';
 import { AboutPage } from './pages/AboutPage';
-import { ServicesPage } from './pages/ServicesPage';
+import { ProgramsPage } from './pages/ProgramsPage';
 import { ServiceDetailPage } from './pages/ServiceDetailPage';
 import { LocationsPage } from './pages/LocationsPage';
 import { GetInvolvedPage } from './pages/GetInvolvedPage';
@@ -21,7 +21,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/services" element={<ProgramsPage />} />
             <Route path="/services/:slug" element={<ServiceDetailPage />} />
             <Route path="/locations" element={<LocationsPage />} />
             <Route path="/get-involved" element={<GetInvolvedPage />} />
